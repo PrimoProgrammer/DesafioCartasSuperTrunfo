ba964c3a23dead0fc67e8ec8bff5190112f4da08
LuanProgrammer <luan.sanka16@gmail.com>
2025-09-24 01:13:39 +0000

    cadastro de aluno em c

--- cadastroaluno.c
+++ cadastroaluno.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("Digite seu nome: \n");
+    scanf("%s", nome);
+
+    printf("Digite sua matrícula: \n");
+    scanf("%d", &matricula);
+
+    printf("- Nome do aluno: %s\n - Matrícula: %d\n", nome, matricula);           
+    printf("- Idade %d\n - Altura %.2f\n", idade, altura);
+
+    return 0;
+}
